e42160c35febce7ee7a6e6739fe59564d3523fe3
Malvern Madondo <mmadondo@fb.com>
2017-06-20 15:57:08 -0700

    Added tip, total, bill labels and text

--- Tippy/ViewController.swift
+++ Tippy/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        //print("Screen Tapped")
+        view.endEditing(true)  //disbale editing when screen tapped
+    }
 
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        let bill = Double(billField.text!) ?? 0 //parse text to type double, change to nil
+        let tip = bill * 0.2
+        let total = bill + tip
+        tipLabel.text = "$10.00"
+        totalLabel.text = "$100.00"
+    }
 }
 

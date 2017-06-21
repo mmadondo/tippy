4a0757466eb879670e9c4a57d8f2a3f9f6882aea
Malvern Madondo <mmadondo@fb.com>
2017-06-20 18:00:58 -0700

    Added new features

--- Tippy/ViewController.swift
+++ Tippy/ViewController.swift
@@ -14,13 +14,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var numPeopleSplit: UITextField!
+    @IBOutlet weak var numPeopleLabel: UILabel!
     @IBOutlet weak var tipLabel: UILabel!
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        //self.view.backgroundColor = UIColor.blue  //background color
+        self.view.backgroundColor = UIColor.orange  //background color
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,9 +36,14 @@ class ViewController: UIViewController {
         view.endEditing(true)  //disbale editing when screen tapped
     }
 
+    @IBAction func numPeople(_ sender: Any) {
+      //  let numPeople = Int(
+    }
+    
+    
     @IBAction func calculateTip(_ sender: Any) {
         
-        let tipPercentages = [0.18, 0.2, 0.25]
+        let tipPercentages = [0.10, 0.2, 0.25]
         
         
         let bill = Double(billField.text!) ?? 0 //parse text to type double, change to nil
@@ -46,5 +54,18 @@ class ViewController: UIViewController {
         tipLabel.text = String(format: "$%.2f", tip) //set tip
         totalLabel.text = String(format: "$%.2f", total) //set total
     }
+    
+    @IBAction func btnSplitCost(_ sender: Any) {
+         let tipPercentages = [0.10, 0.2, 0.25]
+        let bill = Double(billField.text!) ?? 0 //parse text to type double, change to nil
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let totalAmnt = bill + tip  //calculate total amount
+        
+        let amntOwed = totalAmnt / Double(numPeopleSplit.text!)!
+        
+        numPeopleLabel.text = String(format: "$%.2f", amntOwed)
+    }
+    
 }
 
